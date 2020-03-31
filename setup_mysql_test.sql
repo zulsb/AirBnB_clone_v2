de6b11288d8769ae1bd2ddf129678c655fe5e3bb
Luz <zulsb2093@gmail.com>
2020-03-31 04:22:07 +0000

    Add script that prepares a MySQL setup test.

---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+-- Script that prepares a MySQL setup test for the project.
+-- Create a database, new user, password with all privileges on the database for test.
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
+USE hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+USE performance_schema;
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
