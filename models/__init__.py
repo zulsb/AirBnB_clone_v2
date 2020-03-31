2f149ec6967118bb3eacac8448b888672ddb5a68
Jdpa357 <jdpa352@hotmail.es>
2020-03-31 18:08:50 +0000

    Comitting changes to multiple model files and adding new engine for the database

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance for your application"""
-from models.engine.file_storage import FileStorage
+from os
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -10,5 +10,12 @@ from models.place import Place
 from models.review import Review
 
 
-storage = FileStorage()
-storage.reload()
+if "HBNB_TYPE_STORAGE" in os.environ:
+    if os.environ['HBNB_TYPE_STORAGE'] == 'db':
+        from models.engine.db_storage import DBStorage
+        storage = DBStorage()
+        storage.reload()
+    elif os.environ['HBNB_TYPE_STORAGE'] == 'fs':
+        from models.engine.file_storage import FileStorage
+        storage = FileStorage()
+        storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,11 +1,35 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel
+import os
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
 
 
-class State(BaseModel):
-    """This is the class for State
-    Attributes:
-        name: input name
+class State(BaseModel, Base):
     """
-    name = ""
+    This is the class for State objects.
+    It is associated with the SQL table 'states'.
+    Attributes:
+        name: String, 128 characters
+    """
+    __tablename__ = 'states'
+
+    name = Column(String(128),
+                  nullable=False)
+
+    if 'HBNB_TYPE_STORAGE' in os.environ:
+        if os.environ['HBNB_TYPE_STORAGE'] == 'db':
+            cities = relationship('City',
+                                  cascade='delete, delete-orphan',
+                                  backref='state')
+        elif os.environ['HBNB_TYPE_STORAGE'] == 'fs':
+            @property
+            def cities(self):
+                """Property getter of list of city instances
+                where state_id equals current State.id"""
+                city_dict = models.storage.all(City)
+                return [city for city in city_dict.values()
+                        if city.state_id == self.id]
