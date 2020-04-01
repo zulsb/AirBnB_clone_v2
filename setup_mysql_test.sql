700b7dab5f077de495a898ea98accf0aac57b05d
Luz <zulsb2093@gmail.com>
2020-04-01 22:50:16 +0000

    Remove create user.

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,8 @@
 -- Script that prepares a MySQL setup test for the project.
 -- Create a database, new user, password with all privileges on the database for test.
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
 USE hbnb_test_db;
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
