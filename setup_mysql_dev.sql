f3e842e8d5b0d4bb002afc6507d49e06c2d6bd25
Luz <zulsb2093@gmail.com>
2020-04-01 22:49:03 +0000

    Remove create user.

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,8 @@
 -- Script that prepares a MySQL server for the project.
 -- Create a database, new user, password with all privileges on the database.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
 USE hbnb_dev_db;
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
