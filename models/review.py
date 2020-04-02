75e51e4be3b74e9fac5a18b8c64bb087a305fd7d
Luz <zulsb2093@gmail.com>
2020-04-02 19:39:21 +0000

    Add DBStorage - Review

--- models/review.py
+++ models/review.py
@@ -12,6 +12,6 @@ class Review(BaseModel):
         text: review description
     """
     __tablename__ = 'reviews'
+    text = Column(String(1024), nullable=False)
     place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    text = Column(String(1024), nullable=False)
