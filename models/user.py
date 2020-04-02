bd6d0bd933edebe952297a1b3794a064d0ca7cbf
Jdpa357 <jdpa352@hotmail.es>
2020-04-02 15:02:26 +0000

    Adding changes to user model

--- models/user.py
+++ models/user.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """This is the user class"""
-from models.base_model import BaseModel
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
 
 
-class User(BaseModel):
+class User(BaseModel, Base): 
     """This is the class for user
     Attributes:
         email: email address
@@ -11,7 +14,18 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
+    if 'HBNB_TYPE_STORAGE' in os.environ:
+        if os.environ['HBNB_TYPE_STORAGE'] == 'db':
+            # TODO implement the deletion requirement
+            places = relationship('Place',
+                                  cascade='delete, delete-orphan',
+                                  backref='user')
+            reviews = relationship('Review',
+                                   cascade='delete, delete-orphan',
+                                   backref='user')
