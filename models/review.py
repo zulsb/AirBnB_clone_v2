7059822816c76a717c3f54f7b3af7861ba09e548
Luz <zulsb2093@gmail.com>
2020-04-02 19:28:09 +0000

    Add DBStorage - Review

--- models/review.py
+++ models/review.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This is the review class"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel):
@@ -10,6 +11,7 @@ class Review(BaseModel):
         user_id: user id
         text: review description
     """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = 'reviews'
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    text = Column(String(1024), nullable=False)
