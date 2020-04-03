aba84e9edad4676112789f07b6d289f3159deca9
Luz <zulsb2093@gmail.com>
2020-04-03 00:04:13 +0000

    Fix MySQL setup

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,5 @@
 -- Script that prepares a MySQL setup test for the project.
 -- Create a database, new user, password with all privileges on the database for test.
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-USE hbnb_test_db;
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-USE performance_schema;
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
