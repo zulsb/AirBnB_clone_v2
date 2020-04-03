fea72b415d1beda81d30511502db1e3b348ef6cc
Luz <zulsb2093@gmail.com>
2020-04-03 00:32:09 +0000

    Change comments

--- models/amenity.py
+++ models/amenity.py
@@ -7,15 +7,13 @@ from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """
-    This is the class for Amenity objects.
-    It is associated with the SQL table 'amenities'.
+    """This is the class for Amenity
     Attributes:
-        name: non-Null String, 128 characters
+        name: input name.
     """
-    __tablename__ = 'amenities'
+    __tablename__ = "amenities"
 
     name = Column(String(128),
                   nullable=False)
-    place_amenities = relationship('Place',
-                                   secondary='place_amenity')
+    place_amenities = relationship("Place",
+                                   secondary="place_amenity")
