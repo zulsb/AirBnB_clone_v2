eed749b4212a00d6b86cdbf8edb43a97005a45ce
Luz <zulsb2093@gmail.com>
2020-04-11 22:34:12 +0000

    Add Fabric script that generates a .tgz archive.

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that contains do_pack function."""
+from fabric.api import local
+from time import datetime
+
+
+def do_pack():
+    """Generate .tgz archive of web_static/ folder."""
+    timenow = datetime.now()
+    try:
+        local("mkdir -p versions")
+        filename = "versions/web_static_{}.tgz".format(timenow)
+        local("tar -cvzf {} web_static/".format(filename))
+        return filename
+    except:
+        return None
