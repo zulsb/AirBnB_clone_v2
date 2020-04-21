9c3bf30f0c68ee21a9a6aabd2d22554bef26d059
Luz <zulsb2093@gmail.com>
2020-04-21 03:44:44 +0000

    Add script that starts a Flask web application with Python is cool!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -24,9 +24,9 @@ def c(text):
     return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python', defaults={"text": "is cool"})
-@app.route('/python/<text>', strict_slashes=False)
-def python(text):
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def py_r(text="is cool"):
     """Module to display custom text2"""
     return "Python {}".format(text.replace("_", " "))
 
